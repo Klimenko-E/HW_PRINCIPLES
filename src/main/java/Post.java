592c51e61911c081818f52eea6402b5e6b6a5fd4
Klimenko.E <klimenko.e@bk.ru>
2023-07-26 07:49:44 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для подписан/неподписан
+    public FormDate birthday;
+
+
+}
